0a807dcefa962c855aa30184b3de8ef388f1c1a3
umer khan <umer.kdev@gmail.com>
2019-01-19 23:41:27 +0200

    adding initial files

---
+++ src/components/Todos.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="todos"><h1>Todo</h1></div>
+</template>
+
+<script>
+export default {
+  name: "Todos"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
