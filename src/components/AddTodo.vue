b430112175cee584057e2dd74f6d086ec0e7a5f1
umer khan <umer.kdev@gmail.com>
2019-01-30 23:41:59 +0200

    adding add todo

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <input type="text" v-model="addtodo"/> 
+    <button @click="addValue">Add</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddTodo",
+  data(){
+      return{
+          addtodo: ''
+      }
+  },
+  methods: {
+      addValue(){
+          this.$emit('add-value', this.addtodo)
+          this.addtodo = ''
+      }
+  }
+};
+</script>
+
+<style>
+</style>
